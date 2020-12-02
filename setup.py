cc1b6b3b75b469809dd5083de5a60a7b14aaf85d
goncharovush <devnull@localhost>
2020-12-02 17:46:11 +0300

    updated requirementrs

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     packages=find_packages(),
     install_requires=[
         "PyQt5>=5.8.2, <=5.15.0",
-        "numpy",
-        "PythonQwt",
-        "dataclasses"
+        "numpy==1.14.5",
+        "PythonQwt==0.8.3",
+        "dataclasses==0.8"
     ]
 )
