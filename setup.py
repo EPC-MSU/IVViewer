f73269f8472b175087240eaf970813f7be3d8d11
vladimirov_iy vladimirov_iy <vladimirov_iy vladimirov_iy@physlab.ru>
2021-07-28 07:25:00 +0300

    Changed version of numpy to resolve conflict with epcore

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         "PyQt5>=5.8.2, <=5.15.0",
-        "numpy==1.14.5",
+        "numpy==1.18.1",
         "PythonQwt==0.8.3",
         "dataclasses==0.8"
     ]
