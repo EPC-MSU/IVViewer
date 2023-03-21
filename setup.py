4077b577e3e192ae6f534e028e301fde8f4e6100
timerke <timerke@mail.ru>
2023-03-21 14:56:39 +0300

    Fixed requirements

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ setup(
     version="0.1.13",
     name="ivviewer",
     packages=find_packages(),
+    python_requires = ">=3.6, <=3.9.13",
     install_requires=[
-        "dataclasses==0.8"
+        "dataclasses==0.8",
         "numpy==1.18.1",
-        "PyQt5>=5.8.2, <=5.15.0",
+        "PyQt5>=5.8.2, <=5.15.2",
+        "PyQt5-sip>=12.0",
         "PythonQwt==0.8.3",
     ],
     package_data={"ivviewer": ["media/*"]}
