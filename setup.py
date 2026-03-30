e07b69f9114d7311ac688bf776b000c1e0d4a337
timerke <timerke@mail.ru>
2026-03-30 16:53:15 +0300

    Version increment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 
 setup(name="ivviewer",
-      version="1.0.7",
+      version="1.0.8",
       description="A configurable Qt widget that displays IV curves",
       url="https://gitlab.ximc.ru/eyepoint/ivviewer",
       author="EPC MSU",
